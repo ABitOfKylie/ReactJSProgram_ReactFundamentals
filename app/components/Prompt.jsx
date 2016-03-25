0107c3a339fdda0504281da1326cb2a03799c112
somini <somini@users.noreply.github.com>
2016-03-25 11:07:31 +0000

    Stateless Functional Components
    Turn the UI component into a simple function, with extra properties.
    This makes rendering purely functional.

--- app/components/Prompt.jsx
+++ app/components/Prompt.jsx
@@ -4,41 +4,41 @@ var PropTypes = React.PropTypes;
 
 var styles = require('../styles');
 
-var Prompt = React.createClass({
-	propTypes: {
-		header: PropTypes.string.isRequired,
-		username: PropTypes.string.isRequired,
-		onSubmit: PropTypes.func.isRequired,
-		onUpdate: PropTypes.func.isRequired,
-	},
-	render: function() {
-		return (
-			<div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={styles.transparentBg}>
-				<h1>{this.props.header}</h1>
-				<div className="col-sm-12">
-					<form onSubmit={this.props.onSubmit}>
-						<div className="form-group">
-							<input
-								className="form-control"
-								placeholder="GitHub Username"
-								type="text"
-								onChange={this.props.onUpdate}
-								value={this.props.initialValue}
-							/>
-						</div>
-						<div className="form-group col-sm-4 col-sm-offset-4">
-							<button
-								className="btn btn-block btn-success"
-								type="submit"
-							>
-								Continue
-							</button>
-						</div>
-					</form>
-				</div>
+function Prompt(props) {
+	return (
+		<div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={styles.transparentBg}>
+			<h1>{props.header}</h1>
+			<div className="col-sm-12">
+				<form onSubmit={props.onSubmit}>
+					<div className="form-group">
+						<input
+							className="form-control"
+							placeholder="GitHub Username"
+							type="text"
+							onChange={props.onUpdate}
+							value={props.initialValue}
+						/>
+					</div>
+					<div className="form-group col-sm-4 col-sm-offset-4">
+						<button
+							className="btn btn-block btn-success"
+							type="submit"
+						>
+							Continue
+						</button>
+					</div>
+				</form>
 			</div>
-		)
-	}
-});
+		</div>
+	)
+};
+
+Prompt.propTypes = {
+	header: PropTypes.string.isRequired,
+	initialValue: PropTypes.string.isRequired,
+	onSubmit: PropTypes.func.isRequired,
+	onUpdate: PropTypes.func.isRequired,
+};
+
 
 module.exports = Prompt;
