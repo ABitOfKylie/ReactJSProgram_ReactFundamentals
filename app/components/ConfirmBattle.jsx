6f427964340c16c0f70297f6686166851e6b6c20
somini <somini@users.noreply.github.com>
2016-03-30 22:38:31 +0100

    Create user details component
    With a wrapper and everything.

--- app/components/ConfirmBattle.jsx
+++ app/components/ConfirmBattle.jsx
@@ -4,6 +4,8 @@ var ReactRouter = require('react-router');
 var Link = ReactRouter.Link;
 
 var styles = require('../styles');
+var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
 
 function puke(obj) {
 	return (
@@ -22,29 +24,27 @@ function ConfirmBattle(props) {
 			<div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
 				<h1>Confirm Players</h1>
 				<div className="col-sm-8 col-sm-offset-2">
-					<div className="col-sm-6">
-						<p className="lead">Player 1</p>
-            {puke(props.playersInfo[0])}
-					</div>
-					<div className="col-sm-6">
-						<p className="lead">Player 2</p>
-            {puke(props.playersInfo[1])}
-						</div>
+					<UserDetailsWrapper>
+						<UserDetails info={props.playersInfo[0]} header='Player 1'/>
+					</UserDetailsWrapper>
+					<UserDetailsWrapper>
+						<UserDetails info={props.playersInfo[1]} header='Player 2'/>
+					</UserDetailsWrapper>
 				</div>
-        <div className="col-sm-8 col-sm-offset-2">
-          <div className="col-sm-12">
-            <button type='button' className='btn btn-lg btn-success' onClick={props.onConfirm}>
-              Fight!
-            </button>
-          </div>
-          <div className="col-sm-12">
-            <Link to='/playerOne'>
-              <button className="btn btn-lg btn-danger">
-                Select other Players
-              </button>
-            </Link>
-          </div>
-        </div>
+				<div className="col-sm-8 col-sm-offset-2">
+					<div className="col-sm-3 col-sm-offset-3">
+						<button type='button' className='btn btn-lg btn-success' onClick={props.onConfirm} style={styles.spacing}>
+							Fight!
+						</button>
+					</div>
+					<div className="col-sm-3">
+						<Link to='/playerOne'>
+							<button className="btn btn-lg btn-danger" style={styles.spacing}>
+								Select other Players
+							</button>
+						</Link>
+					</div>
+				</div>
 			</div>
 		);
 	}
--- app/styles/index.js
+++ app/styles/index.js
@@ -1,7 +1,14 @@
 var styles = {
 	transparentBg: {
 		background: 'transparent',
-	}
+	},
+	img: {
+		maxHeight: '300px',
+		minHeight: '300px',
+	},
+	spacing: {
+		marginTop: '25px',
+	},
 }
 
 module.exports = styles;
