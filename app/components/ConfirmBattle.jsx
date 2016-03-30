f50338852a947e4a5b88e45c77e33694fe6b5d39
somini <somini@users.noreply.github.com>
2016-03-30 20:14:30 +0100

    Code beautification

--- app/components/ConfirmBattle.jsx
+++ app/components/ConfirmBattle.jsx
@@ -4,7 +4,9 @@ var PropTypes = React.PropTypes;
 var styles = require('../styles');
 
 function puke(obj) {
-	return <pre>{JSON.stringify(obj, null, ' ')}</pre>
+	return (
+		<pre>{JSON.stringify(obj, null, ' ')}</pre>
+	);
 
 }
 
@@ -12,11 +14,11 @@ function ConfirmBattle(props) {
 	if (props.isLoading === true) {
 		return (
 			<p>Loading ...</p>
-		)
+		);
 	} else {
 		return (
 			<div>Confirm Battle!: {puke(props)}</div>
-		)
+		);
 	}
 }
 
